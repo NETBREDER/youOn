fe360212e246cc069c7fee8a730110d5348820bc
NETBREDER <webmaster@davidbreder.com>
2021-07-25 19:01:40 -0300

    Update youon-custom-functions.php

--- youon-custom-functions.php
+++ youon-custom-functions.php
@@ -24,14 +24,25 @@ add_action( 'login_enqueue_scripts', 'youon_login_css' );
 
 function youon_login_css() { ?>
     <style>
+		body {
+			background: linear-gradient(to right, #cd2970 0%, #e96140 100%) !important;
+		}
         #login h1 a, .login h1 a {
-            background-image: url(https://youon.digital/wp-content/uploads/2021/06/youon-logo-purple-300.png);
-            height:auto;
-            width:161px;
+            background-image: url(https://youon.digital/wp-content/uploads/2021/07/logo-voceonline-by-apprank-white.png);
+            height:41px;
+            width:240px;
             background-size: 100%;
             background-repeat: no-repeat;
             padding-bottom: 30px;
         }
+		.login form {
+			border: none !important;
+			border-radius: 8px;
+			box-shadow: 0 16px 26px 0 rgba(0,0,0,0.14) !important;
+		}
+		.login #backtoblog a, .login #nav a {
+			color: #000 !important;
+		}
     </style>
 <?php }
 
